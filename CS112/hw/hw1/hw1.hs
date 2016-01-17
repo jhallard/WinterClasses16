d4a93490ce39c24aecec3f247d46216ce701c78b
John Allard <john@jhallard.com>
2016-01-17 14:56:23 -0800

    Almost halfway finished with HW1

--- CS112/hw/hw1/hw1.hs
+++ CS112/hw/hw1/hw1.hs
@@ -5,20 +5,32 @@
 -- | Homework #1 |--
 
 import Data.Char
+import Data.List
 
--- | Write a function that turns a first and last name into "$last, $first"
+-- | 1.) Write a function that turns a first and last name into "$last, $first"
 citeAuthor :: String -> String -> String
 citeAuthor first last = last ++ ", " ++ first
 
--- | Write a function that produces the initials given a first and last name
+-- | 2.) Write a function that produces the initials given a first and last name
 initials :: String -> String -> String
 initials first last = [toUpper (head first) ] ++ "." ++ [toUpper (head last)] ++ "."
 
--- | Take an (Author, title, date) tuple and return the title
+-- | 3.) Take an (Author, title, date) tuple and return the title
 title :: (String, String, Int) -> String
 title (author, title, date) = title
 
--- | Take a author, title, date tuple and return a citation-formatted string
+-- | 4.) Take a author, title, date tuple and return a citation-formatted string
 citeBook :: (String, String, Int) -> String
 citeBook (author, title, date) = title ++ " (" ++ author ++ ", " ++ (show date) ++ ")"
+
+-- | 5.) Write a function that takes a list of citation-tuples and formats them as 
+--       the last function did, except putting a newline between each formatted citation
+bibliography_rec :: [(String, String, Int)] -> String
+bibliography_rec [] = ""
+bibliography_rec (x:y) = citeBook x ++ "\n" ++ bibliography_rec y 
+    
+-- | 6.) Write a function that returns the average publication year from a list of books
+averageYear :: [(String, String, Int)] -> Int
+averageYear x = sum (map (\(x, y, z) -> z) x) `div`  genericLength x
+
 
